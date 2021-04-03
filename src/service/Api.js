89711ae00b0d5c66ad1b83b873002b3c1be88254
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-04-03 19:42:49 -0300

    atualização

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://back-end-pokemon.herokuapp.com";
+const url = "https://back-end-pokemon.herokuapp.com";
 
 const getAllPokemons = () => {
   return axios(`${url}/api/pokemon`);
